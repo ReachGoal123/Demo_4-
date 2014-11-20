b084ed18542210a75974dee5707e9b756a9cfcf8
tarena <tarena@tarenadeiMac.local>
2014-11-19 22:29:36 -0800

    Initial Commit

---
+++ Demo_4 单例类/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Demo_4 单例类
+//
+//  Created by tarena on 14-11-19.
+//  Copyright (c) 2014年 tarena. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
